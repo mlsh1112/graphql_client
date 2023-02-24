e16f6fe2c4c98d8201d11acd09b956325dfa75c2
이서현 <seohyun.lee@lotte.net>
2023-02-24 14:32:21 +0900

    connect apollo client

--- src/routes/Movies.tsx
+++ src/routes/Movies.tsx
@@ -1,9 +1,27 @@
-import React from 'react'
+import { gql, useApolloClient } from '@apollo/client';
+import React, { useEffect, useState } from 'react'
 
 function Movies() {
-  return (
-    <div>This is a list of movies</div>
-  )
+    const [movies, setMovies]=useState([]);
+    const client = useApolloClient();
+
+    useEffect(()=>{
+        client.query({
+            query : gql`
+            {
+                allMovies{
+                    title
+                }
+            }
+            `
+        }).then(res=>setMovies(res.data.allMovies));
+    },[]);
+        
+    return (
+        <div>
+            {movies.map((movie: any) => <li key={movie.id}>{movie.title}</li>)}
+        </div>
+    )
 }
 
 export default Movies
